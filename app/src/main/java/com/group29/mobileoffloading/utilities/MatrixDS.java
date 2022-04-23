c72718499f0203e1353e306b5417ec8e512dd626
vaibhavalreja <valreja@asu.edu>
2022-04-22 23:26:21 -0700

    changed work threshold

--- app/src/main/java/com/group29/mobileoffloading/utilities/MatrixDS.java
+++ app/src/main/java/com/group29/mobileoffloading/utilities/MatrixDS.java
@@ -1,13 +1,15 @@
 package com.group29.mobileoffloading.utilities;
 
+import java.util.Random;
+
 public class MatrixDS {
     public static int[][] createMatrix(int rowCount, int colCount){
         int [][] matrix = new int[rowCount][colCount];
+        Random rand = new Random();
 
         for(int i = 0; i < rowCount; i++){
             for(int j = 0 ; j < colCount; j++){
-                matrix[i][j] = (i + j) % 128;
-                //TODO: Not really sure why this but okay
+                matrix[i][j] = rand.nextInt(200);
             }
         }
         return matrix;
